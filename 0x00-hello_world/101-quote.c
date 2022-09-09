f876281c0836f036672f4802612642fbd90489fd
Senpero <tsenkoomhomsuk@gmail.com>
2022-09-09 16:19:29 -0700

    Task done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
+ *
  * Return: Always 1 (success)
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char str1 = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+        write(2, str1, 59);
 	return (1);
 }
