91e7a35e52ab4997023e3552d8b2460dc03423da
Senpero <tsenkoomhomsuk@gmail.com>
2022-09-12 17:58:07 -0700

    Task 11 done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Program prints all possible different combinations of
+ * three digits numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a <= 9; a++)
+	{
+		for (b = '0'; b <= 9; b++)
+		{
+			for (c = '0'; c <= 9; c++)
+			{
+				if (b > a && c > b)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != '7' || b != '8' || c != '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
