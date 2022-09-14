657fa884535db628732e8cc55cac112438c00bfc
Senpero <tsenkoomhomsuk@gmail.com>
2022-09-13 17:23:37 -0700

    Task 1 done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int m;
+
+	for (m = 'a'; m <= 'z'; m++)
+	{
+		putchar('m');
+	}
+	putchar('\n');
+
+	return (0);
+}
