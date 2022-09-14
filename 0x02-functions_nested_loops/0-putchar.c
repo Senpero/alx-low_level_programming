3aa749a7014d6031c037a443f4699e3ba3258666
Senpero <tsenkoomhomsuk@gmail.com>
2022-09-13 17:08:29 -0700

    Task 0 done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints _putchar
  *
  * Return: Always 0
  */
 int main(void)
 {
-	int a = _putchar;
-
-	printf("Display the word: %d.\n"; a);
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
 
 	return (0);
 }
