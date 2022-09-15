142b2549f0add8de43d66ba7be0ea8c446690ad9
Senpero <tsenkoomhomsuk@gmail.com>
2022-09-15 09:40:30 -0700

    Task 4 done

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,8 +2,9 @@
 
 /**
  * print_most_numbers - a function that prints the numbers, from 0 to 9
+ * except 2 and 4
  *
- * Return: Always 0
+ * Return: 0 for success
  */
 void print_most_numbers(void)
 {
