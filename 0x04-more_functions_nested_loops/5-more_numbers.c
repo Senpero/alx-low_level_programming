e878fd99a0450b35d68094b75c92edb5c6ee6369
Senpero <tsenkoomhomsuk@gmail.com>
2022-09-15 10:04:06 -0700

    Task 5 done

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,11 +7,8 @@
  */
 void more_numbers(void)
 {
-	int a;
 	int b;
 
-	for (a = 0; a < 10; a++)
-	{
 	for (b = 0; b <= 14; b++)
 	{
 		if (b >= 10)
@@ -23,7 +20,6 @@ void more_numbers(void)
 		{
 			_putchar(b + '0');
 		}
-	}
 	}
 	_putchar('\n');
 }
