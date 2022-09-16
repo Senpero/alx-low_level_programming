f306be260be69d383ec99d68d34326488bd6a242
Senpero <tsenkoomhomsuk@gmail.com>
2022-09-15 17:34:22 -0700

    Task 8 done

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - function prints square of length size
+ * @size: length of square integer variable
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size; b++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
