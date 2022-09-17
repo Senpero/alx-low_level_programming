eb2ed6be8cffca0be18a0bfbc08762a5faf14a13
Senpero <tsenkoomhomsuk@gmail.com>
2022-09-16 18:49:25 -0700

    Task 12 done

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer values
+ * @n: type integer variable
+ * Return: void
+ */
+void print_number(int n)
+{
+	_putchar(n + '0');
+}
