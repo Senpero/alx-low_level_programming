62a87a79027c85f18f79e5ac4e334bb7aabb33d4
Senpero <tsenkoomhomsuk@gmail.com>
2022-09-22 17:20:19 -0700

    Task 3 done

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0 if matching, and ns1 - ns2 if not matching
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+		if (*s1 == '\0')
+			return (0);
+	}
+
+	return (*s1 - *s2);
+}
