4f5cce82f43862f33188fc0eaaf338a44fa39f67
Senpero <tsenkoomhomsuk@gmail.com>
2022-09-27 15:16:05 -0700

    Task 5 done

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strstr - description
+ * @haystack: string
+ * @needle: pointer
+ * Return: pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] > '\0'; i++)
+	{
+		if (haystack[j] != needle[j - i])
+		{
+			break;
+		}
+	}
+	if (needle[j - i] == '\0')
+	{
+		return (haystack + i);
+	}
+	return (0);
+}
